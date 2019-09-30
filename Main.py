a58919619bc33138cb1b2d4146a7b277132f5483
samuglz6 <samu_sgl_96@hotmail.com>
2019-09-30 20:23:19 +0200

    md5 generation

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
-import json, pprint
+import json
+import pprint
 import os
-
+import hashlib
 
 def jsonReading():
     for file in os.listdir('.'):
@@ -17,5 +18,17 @@ def jsonReading():
 
     return data
 
+def generateMD5():
+    cadena = ''
+    for value in json_file.values():
+        for x in value:
+                cadena += ''.join(map(str,x))
+
+    print("string: "+cadena)
+    md5 = hashlib.md5(cadena.encode('utf-8')).hexdigest()
+    print("md5: "+md5)
+
 if __name__ == '__main__':
-    pprint.pprint(jsonReading())
+    json_file = jsonReading()
+    pprint.pprint(json_file)
+    generateMD5()
