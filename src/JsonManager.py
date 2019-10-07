dac793931456bd8cfc8b7cc08b4900d54d571dbd
samuglz6 <samu_sgl_96@hotmail.com>
2019-10-07 17:24:12 +0200

    fixed writing json + output folder

--- src/JsonManager.py
+++ src/JsonManager.py
@@ -4,23 +4,23 @@ import os
 import json
 
 class JsonManager():
-    
+
     def jsonReading():
         print("Select the json file:")
         for file in os.listdir('../json'):
             if os.path.splitext(file)[1] == ".json":
                 print('-'+os.path.splitext(file)[0])
-                
+
         selected = input("Selected file:")
-        json_file = '../json/'+selected+'.json'     
+        json_file = '../json/'+selected+'.json'
 
         if json_file:
-            with open(json_file) as output:
+            with open(json_file, 'r') as output:
                 data = json.load(output)
-
+                output.close()
         return data
 
     def jsonWriting(name, cube):
-        with open(name+'.json','w') as file:
+        with open('../output/'+name+'.json','w+') as file:
             json.dump(cube.Faces, file)
-        
+            file.close()
