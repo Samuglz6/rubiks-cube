9f44de0ce4d0d0ff96cd737f11083d8e9a8f196d
samuglz6 <samu_sgl_96@hotmail.com>
2019-10-09 19:49:13 +0200

    Frontier class
    Co-Authored-By: null <BekaBekeri@users.noreply.github.com>

---
+++ src/Frontier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+from TreeNode import TreeNode
+
+def Frontier():
+    def __init__:
+        self.frontier = create()
+
+    def create():
+        return {}
+
+    def insert(node):
+        self.frontier.append(node)
+
+    def remove():
+        self.frontier.remove
+
+    def isEmpty():
+        return not self.frontier
